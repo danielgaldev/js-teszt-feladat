cb5849f5dbf1a7d3d4095980b6f12f83e937068d
Daniel Gal <daniel.gal@sch.bme.hu>
2019-08-14 21:53:53 +0200

    Syntactic magic

--- app/src/components/TodoItem.js
+++ app/src/components/TodoItem.js
@@ -4,32 +4,47 @@ import { Trash, Up, Down } from 'grommet-icons'
 import moment from 'moment'
 
 
-const TodoItem = (props) => (
-  <CheckBox
-    checked={props.todo.completed}
-    label={<Grid fill='horizontal' as='li' columns={['auto', 'flex']} pad='small'>
-      <Box direction='row' gap='small' align='center' pad='xsmall'>
-        <Text>{props.todo.text}</Text>
-        <Text size='xsmall'>{moment(props.todo.date).format('YYYY. MMMM D.')}</Text>
-      </Box>
-      <Box direction='row' justify='end' gap='small'>
-        {
-          !(props.position === 'first') ?
-            <Button plain icon={<Up />} onClick={() => { props.moveTodo(props.index, 'up') }} /> :
-            null
-        }
-        {
-          !(props.position === 'last') ?
-            <Button plain icon={<Down />} onClick={() => { props.moveTodo(props.index, 'down') }} /> :
-            null
-        }
-        <Button plain icon={<Trash />} onClick={() => { props.deleteTodo(props.index) }} />
-      </Box>
-    </Grid>}
-    onChange={event => {
-      props.checkTodo(props.index);
-    }}
-  />
-)
+const TodoItem = (props) => {
+  const upArrow = <Button plain
+    icon={<Up />}
+    key={'up'}
+    onClick={() => { props.moveTodo(props.index, 'up') }} />
+  const downArrow = <Button plain
+    icon={<Down />}
+    key={'down'}
+    onClick={() => { props.moveTodo(props.index, 'down') }} />
+  const arrows = () => {
+    switch (props.position) {
+      case 'first':
+        return [downArrow]
+      case 'last':
+        return [upArrow]
+      default:
+        return [upArrow, downArrow]
+    }
+  }
+  return (
+    <CheckBox
+      checked={props.todo.completed}
+      label={
+        <Grid fill='horizontal' as='li' columns={['auto', 'flex']} pad='small'>
+          <Box direction='row' gap='small' align='center' pad='xsmall'>
+            <Text>{props.todo.text}</Text>
+            <Text size='xsmall'>
+              {moment(props.todo.date).format('YYYY. MMMM D.')}
+            </Text>
+          </Box>
+          <Box direction='row' justify='end' gap='small'>
+            {arrows().map(arrow => arrow)}
+            <Button plain icon={<Trash />} onClick={() => { props.deleteTodo(props.index) }} />
+          </Box>
+        </Grid>
+      }
+      onChange={event => {
+        props.checkTodo(props.index);
+      }}
+    />
+  )
+}
 
 export default TodoItem
