8bbd57e31f65aeda934b4dca30d12eed37d7a7a0
Daniel Gal <daniel.gal@sch.bme.hu>
2019-08-14 17:52:16 +0200

    Date field with validation

--- app/src/components/AddTodoForm.js
+++ app/src/components/AddTodoForm.js
@@ -1,46 +1,29 @@
-import React, { Component, createRef } from 'react'
-import { Form, FormField, Button, Drop, Box, Grid, Calendar } from 'grommet'
-
-
-class CalendarDrop extends Component {
-  targetRef = createRef();
-
-  componentDidMount() {
-    this.forceUpdate();
-  }
-
-  render() {
-    return (
-        <Box align="center" justify="center">
-          <Box
-            background="dark-3"
-            pad="medium"
-            align="center"
-            justify="start"
-            ref={this.targetRef}
-          >
-            Box
-          </Box>
-          {this.targetRef.current && (
-            <Drop
-              align={{ top: "bottom" }}
-              target={this.targetRef.current}
-            >
-              <Calendar size='small'/>
-            </Drop>
-          )}
-        </Box>
-    );
-  }
-}
+import React from 'react'
+import { Form, FormField, Button, Box, Grid, TextInput } from 'grommet'
+import { Add } from 'grommet-icons'
+import moment from 'moment'
 
 const AddTodoForm = (props) => (
   <Form onSubmit={({ value }) => console.log(value)}>
     <Box round='xsmall' width='large' pad='xsmall' border={{ size: 'xsmall', color: 'brand' }}>
-      <Grid fill gap='xsmall' columns={['flex', 'auto', 'auto']}>
-        <FormField name='text' required />
-        <CalendarDrop />
-        <Button type='submit' primary label='Add' />
+      <Grid gap='xsmall' columns={['flex', 'auto', 'auto']}>
+        <FormField
+          name='text'
+          required
+          component={TextInput}
+          placeholder='Add task'
+          plain />
+        <FormField
+          name='date'
+          required
+          validate={input => {
+            if (!moment(input, 'YYYY/MM/DD', true).isValid())
+              return 'Format: YYYY/MM/DD'
+          }}
+          component={TextInput}
+          placeholder='Date'
+          plain />
+        <Button type='submit' primary icon={<Add />} />
       </Grid>
     </Box>
   </Form>
